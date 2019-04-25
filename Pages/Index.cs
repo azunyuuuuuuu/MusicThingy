ce15655f647c620bfa605e424cce5b60ef0065f8
Alice T <dotnewzero@gmail.com>
2019-04-25 14:08:58 +0200

    part 2, implementing index and reworking

---
+++ Pages/Index.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.EntityFrameworkCore;
+using MusicThingy.Models;
+using YoutubeExplode;
+
+namespace MusicThingy.Pages
+{
+    public class IndexBase : ComponentBase
+    {
+        [Inject] protected DataRepository _repository { get; set; }
+
+        public List<Source> Sources { get; set; } = new List<Source>();
+
+        protected override async Task OnInitAsync()
+        {
+            Sources.AddRange((await _repository.GetAllSources())
+                .OrderBy(x => x.Title));
+        }
+    }
+}
