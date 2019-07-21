a272e1a55d811913c346ae244887334d0183bcee
Alice Triebull <dotnewzero@gmail.com>
2019-07-21 09:33:12 +0200

    fixes

--- Pages/Settings/ManageSourcesBase.cs
+++ Pages/Settings/ManageSourcesBase.cs
@@ -69,6 +69,7 @@ namespace MusicThingy.Pages
         {
             try { await _repository.RemoveSource(source); } catch (Exception) { }
             await RebuildList();
+            StateHasChanged();
         }
 
         public void OpenModal()
