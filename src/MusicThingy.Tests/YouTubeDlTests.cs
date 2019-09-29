bdef9a592476d8ea75b5443c137701bc7001e869
Alice T <azunyuuuuuuu@users.noreply.github.com>
2019-09-29 18:38:35 +0200

    Create tests for the YouTubeDl fetch routines

---
+++ src/MusicThingy.Tests/YouTubeDlTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace MusicThingy.Tests
+{
+    public class YouTubeDlTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [TestCase(@"https://www.youtube.com/watch?v=BaW_jenozKc&t=1s&end=9", @"youtube-dl test video ""'/\ä↭𝕐")]
+        public void Fetch_YouTubeMetaData(string url, string title)
+        {
+            Assert.Pass();
+        }
+    }
+}
